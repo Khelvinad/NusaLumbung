9b027a5d4ae7573aca36d3275896c6a050e994e4
Khelvinad <khelvinadit@gmail.com>
2026-05-10 16:02:18 +0700

    feat: implementasi auth multi-role, profil user, dan pass 100% testing

--- app/Models/PembeliProfile.php
+++ app/Models/PembeliProfile.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class PembeliProfile extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'no_telp',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/PetaniProfile.php
+++ app/Models/PetaniProfile.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PetaniProfile extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'no_telp',
+        'nama_tani',
+        'lokasi',
+        'bio',
+        'rating_avg',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
